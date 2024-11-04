df4416c3e96d367d683f432389badd8b082e4aea
maoude241 <samir.maoude@etu.unicaen.fr>
2024-11-04 14:52:12 +0100

    Main

--- livraison/src/fightGame/src/fightGame/MainClass.java
+++ livraison/src/fightGame/src/fightGame/MainClass.java
@@ -4,7 +4,15 @@ import gamePlayers.fighters.Unit;
 
 public class MainClass {
     public static void main(String[] args) {
-        //Unit unit = new Unit("Toto");
+
+        // Charger les paramètres de jeu depuis le fichier XML
+        UnchangeableSettings.loadSettings();
+
+        // Affichage des paramètres pour vérifier le chargement
+        System.out.println("Nombre de rangées: " + UnchangeableSettings.NB_ROWS);
+        System.out.println("Nombre de bombes: " + UnchangeableSettings.NB_BOMBS);
+
+        
         System.out.println("fightGame package ok");
     }
 }
