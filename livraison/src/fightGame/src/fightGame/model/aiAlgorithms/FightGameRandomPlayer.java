c3232a2f5c7436772c15234753e7417e9e7d9bca
maoude241 <samir.maoude@etu.unicaen.fr>
2024-11-13 19:09:51 +0100

    random player

---
+++ livraison/src/fightGame/src/fightGame/model/aiAlgorithms/FightGameRandomPlayer.java
@@ -0,0 +1,30 @@
+package fightGame.model.aiAlgorithms;
+
+import java.util.List;
+import java.util.Random;
+
+import fightGame.model.FightGamePlayer;
+import fightGame.model.GameBoard;
+import fightGame.model.GameBoardProxy;
+import gamePlayers.util.Action;
+import gamePlayers.util.Position;
+
+public class FightGameRandomPlayer extends FightGamePlayer{
+
+    public FightGameRandomPlayer(GameBoard gameBoard, String name, int playerIndex, Position startingposition){
+        super(gameBoard, name, playerIndex, startingposition);
+        this.gameBoardProxy = new GameBoardProxy(gameBoard, this);
+    }
+
+    @Override
+    public Action play() {
+        List<Action> actions = this.gameBoardProxy.getActions();
+
+        Random random = new Random();
+        Action randomAction = actions.get(random.nextInt(actions.size()));
+        
+
+        return randomAction;
+    }
+
+}
