9a640a50c2236ed9a1b33e07652a083e78b419be
maoude241 <samir.maoude@etu.unicaen.fr>
2024-11-24 17:18:32 +0100

    Minimax improving

--- livraison/src/fightGame/src/fightGame/model/aiAlgorithms/MinimaxStrategy.java
+++ livraison/src/fightGame/src/fightGame/model/aiAlgorithms/MinimaxStrategy.java
@@ -1,10 +1,12 @@
 package fightGame.model.aiAlgorithms;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import fightGame.UnchangeableSettings;
 import fightGame.model.FightGameAction;
+import fightGame.model.FightGameActionType;
 import fightGame.model.FightGamePlayer;
 import fightGame.model.GameBoard;
 import fightGame.model.GameBoardProxy;
@@ -12,6 +14,7 @@ import gamePlayers.fighters.Unit;
 import gamePlayers.util.Action;
 import gamePlayers.util.Direction;
 import gamePlayers.util.Player;
+import gamePlayers.util.Position;
 
 
 
@@ -32,8 +35,7 @@ public class MinimaxStrategy implements FightGamePlayerStrategy,Serializable {
     }
 
 
-    private static final double WEAPONS_WEIGHT = -0.1;
-    private static final double PROXIMITY_WEIGHT = 30;
+    private static final double PROXIMITY_WEIGHT = 1.5;
 
     // Other properties and methods...
 
@@ -48,10 +50,6 @@ public class MinimaxStrategy implements FightGamePlayerStrategy,Serializable {
         // 1. Health/Energy metric: Higher energy is better
         score += currentUnit.getEnergy();
 
-        // 2. Weapons count: Stimulate player to use weapon
-        int totalWeapons = currentUnit.getBombs().size() + currentUnit.getMines().size() + currentUnit.getProjectiles().size();
-        score += WEAPONS_WEIGHT * (totalWeapons);
-
         // 3. Simulate players to move to each other
         double totalDistance = 0.0;
         int nbOpponents = 0;
@@ -63,7 +61,7 @@ public class MinimaxStrategy implements FightGamePlayerStrategy,Serializable {
                 totalDistance += distance;
                 nbOpponents++;
 
-                if(currentUnit.getEnergy() < opponent.getUnit().getEnergy()) score += opponent.getUnit().getEnergy() - currentUnit.getEnergy()  ;
+                // if(currentUnit.getEnergy() < opponent.getUnit().getEnergy()) score += opponent.getUnit().getEnergy() - currentUnit.getEnergy()  ;
             }
 
             
@@ -103,23 +101,114 @@ public class MinimaxStrategy implements FightGamePlayerStrategy,Serializable {
             return new Result(score, null);  // Terminal node, no move to return
         }
 
+        Unit currUnit = currentPlayer.getUnit();
+        List<FightGamePlayer> players = gameBoard.getPlayers();
+
+        // Get all possible moves for the current player
+        List<Action> possibleActions = new ArrayList<>();
+        
+        // Build Attack First Actions
+        Position currPosition = currUnit.getPosition();
+        for (FightGamePlayer opponent : players) {
+            if(opponent.equals(currentPlayer))continue;
+            if(opponent.getUnit() == null) continue;
+
+            Unit oppUnit = opponent.getUnit();
+            
+            if(oppUnit.isAlive()){
+                
+                Position opponentPosition = oppUnit.getPosition();
+                double distance = currPosition.distanceTo(opponentPosition);
+                if(currPosition.getRow() == opponentPosition.getRow()){
+                    if(distance <= UnchangeableSettings.PROJECTILE_SCOPE){
+                        
+                        if(currPosition.getCol()< opponentPosition.getCol()){
+                            if(currUnit.hasProjectiles())possibleActions.add(new FightGameAction(FightGameActionType.USE_PROJECTILE_AT_RIGHT));
+                            if(distance <= 2){
+                                if(currUnit.hasBombs())possibleActions.add(new FightGameAction(FightGameActionType.USE_BOMB_AT_RIGHT));
+                                if(currUnit.hasMines())possibleActions.add(new FightGameAction(FightGameActionType.USE_MINE_AT_RIGHT));
+                            }
+                        }
+                        else{
+                            if(currUnit.hasProjectiles())possibleActions.add(new FightGameAction(FightGameActionType.USE_PROJECTILE_AT_LEFT));
+                            if(distance <= 2){
+                                if(currUnit.hasBombs())possibleActions.add(new FightGameAction(FightGameActionType.USE_BOMB_AT_LEFT));
+                                if(currUnit.hasMines())possibleActions.add(new FightGameAction(FightGameActionType.USE_MINE_AT_LEFT));
+                            }
+                        }
+                    }
+                }
+                else if(currPosition.getCol() == opponentPosition.getCol()){
+                    if(distance <= UnchangeableSettings.PROJECTILE_SCOPE){
+                        if(currPosition.getRow()< opponentPosition.getRow()){
+                            if(currUnit.hasProjectiles())possibleActions.add(new FightGameAction(FightGameActionType.USE_PROJECTILE_AT_BOTTOM));
+                            if(distance <= 2){
+                                if(currUnit.hasBombs())possibleActions.add(new FightGameAction(FightGameActionType.USE_BOMB_AT_BOTTOM));
+                                if(currUnit.hasMines())possibleActions.add(new FightGameAction(FightGameActionType.USE_MINE_AT_BOTTOM));
+                            }
+                        }
+                        else{
+                            if(currUnit.hasProjectiles())possibleActions.add(new FightGameAction(FightGameActionType.USE_PROJECTILE_AT_TOP));
+                            if(distance <= 2){
+                                if(currUnit.hasBombs())possibleActions.add(new FightGameAction(FightGameActionType.USE_BOMB_AT_TOP));
+                                if(currUnit.hasMines())possibleActions.add(new FightGameAction(FightGameActionType.USE_MINE_AT_TOP));
+                            }
+                        }
+                    }
+                }
+            }
+
+            
+            
+        }
+
+
+        if(possibleActions.isEmpty()){
+            possibleActions = gameBoard.getActions(currentPlayer);
+
+            int idx = 0;
+            for(Action action: new ArrayList<>(possibleActions)){
+                FightGameAction fightGameAction = (FightGameAction)action;
+                switch (fightGameAction.getTYPE()) {
+                    case USE_PROJECTILE_AT_RIGHT:
+                    case USE_PROJECTILE_AT_LEFT:
+                    case USE_PROJECTILE_AT_BOTTOM:
+                    case USE_PROJECTILE_AT_TOP:
+                    case USE_BOMB_AT_BOTTOM:
+                    case USE_BOMB_AT_TOP:
+                    case USE_BOMB_AT_BOTTOM_LEFT:
+                    case USE_BOMB_AT_RIGHT:
+                    case USE_MINE_AT_BOTTOM:
+                    case USE_MINE_AT_TOP:
+                    case USE_MINE_AT_BOTTOM_LEFT:
+                    case USE_MINE_AT_RIGHT:
+                        possibleActions.remove(idx);
+                        idx -= 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                idx += 1;
+            }
+        }
+        
+
         int currentPlayerIndex = currentPlayer.getPlayerIndex();
 
         double bestScore = currentPlayerIndex == playerIndex  ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
 
-        
 
         Action bestAction = null;
 
-
-        // Get all possible moves for the current player
-        List<Action> possibleActions = gameBoard.getActions(currentPlayer);
-
         for (Action action : possibleActions) {
+
+            FightGameAction fightGameAction = (FightGameAction)action;
+
             // Apply the move to get the next game state
             GameBoard nextGameBoard = new GameBoard(gameBoard);
             
-            nextGameBoard.performAction((FightGameAction)action, new FightGamePlayer(currentPlayer));
+            nextGameBoard.performAction(fightGameAction, new FightGamePlayer(currentPlayer));
             
             FightGamePlayer nextPlayer = nextGameBoard.getNextPlayer();
             
