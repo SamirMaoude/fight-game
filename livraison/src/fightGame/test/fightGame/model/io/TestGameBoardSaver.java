915bf48429eaea3eb714caaffcfa37ad373a9515
Patrice404 <patricecotcho@gmail.com>
2024-12-01 00:43:00 +0100

    merge

--- livraison/src/fightGame/test/fightGame/model/io/TestGameBoardSaver.java
+++
@@ -1,9 +0,0 @@
-package fightGame.model.io;
-
-import junit.framework.TestCase;
-
-public class TestGameBoardSaver extends TestCase {
-    public void test(){
-        
-    }
-}
--- livraison/src/gamePlayers/src/gamePlayers/MainClass.java
+++
@@ -1,7 +0,0 @@
-package gamePlayers;
-
-public class MainClass {
-    public static void main(String[] args) {
-        System.out.println("ça tourne");
-    }
-}
