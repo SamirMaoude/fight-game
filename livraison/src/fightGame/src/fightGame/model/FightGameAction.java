2ed75a7684cf53d2a83fd46d2d35b93e5afd244f
Aissatou Diallo <diallo2318@CAMPUS@V304V-JCP087.campus.unicaen.fr>
2024-11-30 16:59:56 +0100

    Ajout de screenDisplay

--- livraison/src/fightGame/src/fightGame/model/FightGameAction.java
+++ livraison/src/fightGame/src/fightGame/model/FightGameAction.java
@@ -14,6 +14,87 @@ public class FightGameAction implements Action, Serializable {
     public String toString() {
         return "Action [TYPE=" + TYPE + "]";
     }
+
+    public String screenDisplay(){
+        String display = "";
+        switch (TYPE){
+            case MOVE_UNIT_TO_RIGHT :
+                display = "Aller à droite";
+                break;
+            case MOVE_UNIT_TO_LEFT :
+                display = "Aller à gauche";
+                break;
+            case  MOVE_UNIT_TO_BOTTOM :
+                display = "Aller en bas";
+                break;
+            case MOVE_UNIT_TO_TOP :
+                display = "Aller en haut";
+                break;
+            case USE_MINE_AT_LEFT :
+                display = "Placer une mine à gauche";
+                break;
+            case USE_MINE_AT_RIGHT :
+                display = "Placer une mine à droite";
+                break;
+            case USE_MINE_AT_BOTTOM :
+                display = " Placer une mine en bas";
+                break;
+            case  USE_MINE_AT_TOP :
+                display = "Placer une mine en haut";
+                break;
+            case USE_MINE_AT_TOP_LEFT :
+                display = "Placer une mine en haut à gauche ";
+                break;
+            case  USE_MINE_AT_TOP_RIGHT :
+                display = "Placer une mine en haut à droite";
+                break;
+            case USE_MINE_AT_BOTTOM_LEFT :
+                display = "Placer une mine en bas à gauche ";
+                break;
+            case USE_MINE_AT_BOTTOM_RIGHT :
+                display = "Placer une mine en bas à droite ";
+            case USE_BOMB_AT_LEFT :
+                display = "Placer une bombe à gauche ";
+                break;
+            case USE_BOMB_AT_RIGHT :
+                display = "Placer une bombe à droite ";
+                break;
+            case  USE_BOMB_AT_BOTTOM :
+                display = "Placer une bombe en bas " ;
+                break;
+            case USE_BOMB_AT_TOP :
+                display = "Placer une bombe en haut ";
+                break;
+            case USE_BOMB_AT_TOP_LEFT :
+                display = "Placer une bombe en haut à gauche ";
+                break;
+            case  USE_BOMB_AT_TOP_RIGHT :
+                display = "Placer une bombe en haut à droite ";
+                break;
+            case USE_BOMB_AT_BOTTOM_LEFT :
+                display = "Placer une bombe en bas à gauche ";
+                break;
+            case  USE_BOMB_AT_BOTTOM_RIGHT :
+                display = "Placer une bombe en bas à droite ";
+                break;
+            case USE_PROJECTILE_AT_RIGHT :
+                display = "Tirer à droite ";
+                break;
+            case USE_PROJECTILE_AT_LEFT :
+                display = "Tirer à gauche ";
+                break;
+            case USE_PROJECTILE_AT_TOP :
+                display = "Tirer en haut ";
+                break;
+            case USE_PROJECTILE_AT_BOTTOM :
+                display = "Tirer en bas ";
+                break;
+            default :
+                break;
+        }
+        return display;
+    }
+
     public FightGameActionType getTYPE() {
         return TYPE;
     }
